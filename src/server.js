7ef9fb1fdba2e7c6e121dcd79e3856fd227b67f9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-02-13 11:40:04 -0300

    feat: add randomUUID to create user

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
 
@@ -19,7 +20,7 @@ const server = http.createServer(async (req, res) => {
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     };
