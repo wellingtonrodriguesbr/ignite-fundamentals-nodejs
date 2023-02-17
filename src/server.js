44dabfaa3ca6a1168b3608f19f80b3e8edbbee05
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-02-17 05:46:30 -0300

    feat: capture query parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -10,11 +11,14 @@ const server = http.createServer(async (req, res) => {
     return route.method === method && route.path.test(url);
   });
 
-  console.log(route)
+  console.log(route);
 
   if (route) {
     const routeParams = req.url.match(route.path);
-    req.params = { ...routeParams.groups };
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
